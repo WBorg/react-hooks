51eb46a3189e208f6ceef66bd18e6d8d02e7c0ed
WILLY BORGES SILVA <willy.bsilva@senacsp.edu.br>
2022-04-25 22:16:30 -0300

    Start

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+import React ,  {useEffect, useState} from 'react';
+
+function App() {
+
+  const [name, setName] = useState('Senac Campinas');
+  
+  let interval = null;
+  interval = setInterval(() => {
+  setName(`ReactJS Senac`)
+    
+  }, 5000);
+      // Desta maneira podemos simular o componentDidMount e o componentDidUpdate
+useEffect(() => {
+  document.title = name;
+ 
+
+
+
+  // Aqui estou executando o componentWillUnmount
+  return () => clearInterval(interval);
+}, [name]);
+
+
+
+  return (
+    <div className="App">
+      
+    <h1>Senac</h1>
+
+    </div>
+  );
+}
+
+export default App;
